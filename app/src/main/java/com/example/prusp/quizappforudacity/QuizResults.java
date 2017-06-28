f14421cf5db334615416e9bef97f7e4cf5c390ed
Piotr Prus <prus.piotr@gmail.com>
2017-06-28 22:57:45 +0200

    Add the toast in QuizResult.java

--- app/src/main/java/com/example/prusp/quizappforudacity/QuizResults.java
+++ app/src/main/java/com/example/prusp/quizappforudacity/QuizResults.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizResults extends AppCompatActivity {
 
@@ -24,7 +25,19 @@ public class QuizResults extends AppCompatActivity {
 
     private void updateQuizScore() {
         TextView resultsTextView = (TextView) findViewById(R.id.results_text_view);
-        String result = (String.valueOf(quizScore)+"/6");
+        String result = (String.valueOf(quizScore) + getString(R.string.out_of_6));
         resultsTextView.setText(result);
+        showToastWithResult();
+    }
+
+    private void showToastWithResult() {
+        if (quizScore == 6) {
+            Toast.makeText(this, R.string.toast_text_for_score_6, Toast.LENGTH_LONG).show();
+        } else if (quizScore == 5) {
+            Toast.makeText(this, R.string.toast_text_for_score_5, Toast.LENGTH_LONG).show();
+        } else {
+            String stringForToast = getString(R.string.toast_text_for_score_less_than_5) + quizScore + getString(R.string.out_of_6);
+            Toast.makeText(this, stringForToast, Toast.LENGTH_LONG).show();
+        }
     }
 }
